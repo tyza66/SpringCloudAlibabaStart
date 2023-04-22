b0ed7a59ad63b3fec36a0eddd77faeb5147050c5
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-22 10:25:26 +0800

    update

---
+++ springcloudalibaba/stock-nacos/src/main/java/com/tyza66/stock/StockApplication.java
@@ -0,0 +1,16 @@
+package com.tyza66.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: tyza66
+ * Date: 2023/04/21 22:17
+ * Github: https://github.com/tyza66
+ **/
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class, args);
+    }
+}
