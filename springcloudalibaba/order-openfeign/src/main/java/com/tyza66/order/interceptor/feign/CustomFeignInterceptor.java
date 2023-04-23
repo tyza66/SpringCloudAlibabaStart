39041aa5caab5dfe455cdbe7c0b35b0db7447b00
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-23 10:46:23 +0800

    update

---
+++ springcloudalibaba/order-openfeign/src/main/java/com/tyza66/order/interceptor/feign/CustomFeignInterceptor.java
@@ -0,0 +1,21 @@
+package com.tyza66.order.interceptor.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Author: tyza66
+ * Date: 2023/04/23 10:28
+ * Github: https://github.com/tyza66
+ **/
+public class CustomFeignInterceptor implements RequestInterceptor {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    public void apply(RequestTemplate requestTemplate) {
+        //为当前请求头设置参数
+        requestTemplate.header("xxx","xxx");
+        //为当前请求头修改参数
+        logger.info("feign拦截器");
+    }
+}
