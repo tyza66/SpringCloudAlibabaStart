327b3d39c7749ed2c2f8ef8c2834146be301fd29
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-24 15:07:06 +0800

    update

--- springcloudalibaba/order-sentinel/src/main/java/com/tyza66/order/controller/OrderController.java
+++ springcloudalibaba/order-sentinel/src/main/java/com/tyza66/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.tyza66.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,16 @@ public class OrderController {
     RestTemplate restTemplate;*/
 
     @RequestMapping("/add")
+    @SentinelResource(value = "add", blockHandler = "blockAdd")
     public String add() {
         System.out.println("下单成功!");
         /*//远程请求调用 参数:远程调用的地址,相应的数据类型,参数类型
         String forObject = restTemplate.getForObject("http://localhost:8082/stock/reduce", String.class);
         System.out.println("接收到:"+forObject);*/
         return "Hello,World!";
+    }
+
+    public String blockAdd(BlockException e) {
+        return "流控";
     }
 }
