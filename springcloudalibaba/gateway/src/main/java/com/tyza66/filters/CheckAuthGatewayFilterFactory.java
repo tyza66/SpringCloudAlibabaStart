170c629ca0175b9a81a8b152bfe19ada945a18ff
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-27 07:59:49 +0800

    update

--- springcloudalibaba/gateway/src/main/java/com/tyza66/filters/CheckAuthGatewayFilterFactory.java
+++ springcloudalibaba/gateway/src/main/java/com/tyza66/filters/CheckAuthGatewayFilterFactory.java
@@ -1,8 +1,11 @@
 package com.tyza66.filters;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -14,6 +17,7 @@ import java.util.List;
  * Date: 2023/04/27 7:36
  * Github: https://github.com/tyza66
  **/
+@Component
 public class CheckAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<CheckAuthGatewayFilterFactory.Config> {
 
     public CheckAuthGatewayFilterFactory() {
@@ -21,20 +25,39 @@ public class CheckAuthGatewayFilterFactory extends AbstractGatewayFilterFactory<
     }
 
     public List<String> shortcutFieldOrder() {
-        return Arrays.asList("");
+        return Arrays.asList("value");
     }
 
     public GatewayFilter apply(CheckAuthGatewayFilterFactory.Config config) {
         return new GatewayFilter() {
             @Override
             public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-                return null;
+                //获取参数 如果和value值不相等就失败
+                String name = exchange.getRequest().getQueryParams().getFirst("name");//获得name参数
+                if(StringUtils.isNotBlank(name)){
+                    if(config.getValue().equals(name)){
+                        return chain.filter(exchange);
+                    }else{
+                        //返回404
+                        exchange.getResponse().setStatusCode(HttpStatus.NOT_FOUND);
+                        return exchange.getResponse().setComplete();
+                    }
+                }
+                return chain.filter(exchange);
             }
         };
     }
 
 
     public static class Config {
+        private String value;
+        //上面调用的是这个get方法
+        public String getValue() {
+            return value;
+        }
 
+        public void setValue(String value) {
+            this.value = value;
+        }
     }
 }
