29b076cb45fde80ccae9565b29042c04ae295793
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-27 09:46:34 +0800

    update

---
+++ springcloudalibaba/gateway/src/main/java/com/tyza66/configCORSConfig.java
@@ -0,0 +1,29 @@
+package com.tyza66;
+
+import io.netty.handler.codec.http.cors.CorsConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * Author: tyza66
+ * Date: 2023/04/27 9:37
+ * Github: https://github.com/tyza66
+ **/
+@Configuration
+public class configCORSConfig {
+    @Bean
+    public CorsWebFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");  //允许的method
+        config.addAllowedOrigin("*");  //允许的来源
+        config.addAllowedHeader("*");  //允许的请求头
+        //允许访问的资源
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(source);
+    }
+}
